f380f21e560346b9c47fb4ff00e9916053e9360d
alikhere <alikhurshidhere@gmail.com>
2025-02-01 01:24:31 +0530

    model improved

--- loan_sahayak_api_py.py
+++ loan_sahayak_api_py.py
@@ -21,14 +21,16 @@ origins = [
     "https://loan-sahayak-z5n7.onrender.com",
 ]
 
-# Add CORS middleware
+# Add CORS middlewar
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, OPTIONS, etc.)
+    allow_headers=["*"],  # Allows all headers
 )
+
 
 # Define the request schema
 class PredictionRequest(BaseModel):
@@ -73,4 +75,4 @@ def predict(request: PredictionRequest):
 # Run the application
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8028)
+    uvicorn.run(app, host='0.0.0.0', port=8031)
